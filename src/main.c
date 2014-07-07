0012b6373eb6906487a6fa5ee1e69e90174e78dd
Joaquin de Andres <joaquin@satellogic.com>
2014-07-07 11:32:09 -0300

    Lo que probamos en la reunion del 5 julio

--- src/main.c
+++ src/main.c
@@ -5,34 +5,6 @@
 uint8_t vel_count;
 uint8_t vel_ref;
 
-ISR(INT0_vect){
-    // Debounce
-    _delay_ms(10);
-    if ((SENSOR_HALL_PORT & (1<<SENSOR_HALL_PIN_NUM)) == 0){
-        flashLeds();
-        vel_count += 1;
-    }
-}
-
-/*
-// vel va entre 0 y PWM_MAX (ej: 500)
-// e va entre -PWM_MAX y PWM_MAX
-// vel_count 
-
-#define K1 10
-void PID(uint8_t e, uint8_t* vel, uint8_t prev_vel){
-  *vel = prev_vel + e * (K1/100);
-}
-
-ISR(TIMER_OV){
-  int e;
-  uint8_t vel;
-  e = vel_count - vel_ref;
-  PID(e,&vel,vel_count);
-  setMotorVel(e*k1);
-}
-*/
-
 void setupHallEffectSensor(void){
   ClearBit(SENSOR_HALL_DDR, SENSOR_HALL_PIN_NUM);
   ClearBit(SENSOR_HALL_PORT, SENSOR_HALL_PIN_NUM);
@@ -92,9 +64,36 @@ void setupFlashLeds(void){
   SetBit(FLASH_1_PORT, FLASH_1_PIN_NUM);
 }
 
+#define AD1_PIN_NUM 4
+#define AD2_PIN_NUM 5
+#define AD1_MUX 4
+#define AD2_MUX 5
+uint8_t ad1_var;
+uint8_t ad2_var;
+
 void flashLeds(void){
   toggle_led_1();
-  _delay_ms(FLASH_LEN_MS);
+  if(ad2_var < 20){
+        _delay_ms(20);
+  } else if (ad2_var < 40){
+        _delay_ms(40);
+  } else if (ad2_var < 60){
+        _delay_ms(60);
+  } else if (ad2_var < 80){
+        _delay_ms(80);
+  } else if (ad2_var < 100){
+        _delay_ms(100);
+  } else if (ad2_var < 120){
+        _delay_ms(120);
+  } else if (ad2_var < 140){
+        _delay_ms(140);
+  } else if (ad2_var < 160){
+        _delay_ms(160);
+  } else if (ad2_var < 180){
+        _delay_ms(180);
+  } else {
+        _delay_ms(200);
+  }
   toggle_led_1();
 }
 
@@ -107,15 +106,69 @@ void setupDebugLeds(void){
   SetBit(LED_3_PORT, LED_3_PIN_NUM);
 }
 
+void setupAD(void){
+    ClearBit(DDRC, AD1_PIN_NUM);
+    ClearBit(DDRC, AD2_PIN_NUM);
+    SetBit(PORTC, AD1_PIN_NUM);
+    SetBit(PORTC, AD2_PIN_NUM);
+
+    ADMUX =_BV(ADLAR);
+    ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+}
+
+void readAD(void){
+    ADMUX =_BV(ADLAR)+AD1_MUX;
+    ADCSRA |=_BV(ADSC);
+    while(ADCSRA &_BV(ADSC)) {}
+    ad1_var = ADCH;
+
+    ADMUX =_BV(ADLAR)+AD2_MUX;
+    ADCSRA |=_BV(ADSC);
+    while(ADCSRA &_BV(ADSC)) {}
+    ad2_var = ADCH;
+}
+
 void main(void){
 
   //setupHallEffectSensor();
+  setupAD();
   setupFlashLeds();
   setupMotor();
-  setMotorVel(50);
+  setMotorVel(70);
 
   for(;;){
     flashLeds();
-    _delay_ms(1000);
+    readAD();
+    setMotorVel(ad1_var*100/256);
+    _delay_ms(500);
   }
 }
+
+/*
+ISR(INT0_vect){
+    //Debounce
+    _delay_ms(10);
+    if ((SENSOR_HALL_PORT & (1<<SENSOR_HALL_PIN_NUM)) == 0){
+        flashLeds();
+        vel_count += 1;
+    }
+}*/
+
+/*
+// vel va entre 0 y PWM_MAX (ej: 500)
+// e va entre -PWM_MAX y PWM_MAX
+// vel_count 
+
+#define K1 10
+void PID(uint8_t e, uint8_t* vel, uint8_t prev_vel){
+  *vel = prev_vel + e * (K1/100);
+}
+
+ISR(TIMER_OV){
+  int e;
+  uint8_t vel;
+  e = vel_count - vel_ref;
+  PID(e,&vel,vel_count);
+  setMotorVel(e*k1);
+}
+*/
